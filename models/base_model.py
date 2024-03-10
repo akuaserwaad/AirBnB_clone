8426bd44ed07159c453047e7256b588aaf97a85a
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2024-03-10 03:05:11 -0700

    AirBnB clone - The console

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,80 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+
+"""A module that defines the Base Model"""
+
 from datetime import datetime
+from typing import Any
+from uuid import uuid4
+import models
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Defines the Base Model."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+    def __init__(self, *args, **kwargs) -> None:
+        """Initializes the Base Model."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
 
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+                # ensure the date and time is set correctly
+                if key in ["updated_at", "created_at"]:
+                    value = datetime.fromisoformat(value)
+
+                self.__dict__[key] = value
         else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+    def __str__(self) -> str:
+        """Returns the string representation for an instance of the Base Model.
+
+        Returns:
+            str: The string representation for an instance of the Base Model.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        """Handles the setting of attributes.
+
+        This method updates the `updated_at` attribute whenever a new attribute
+        is added to the instance.
+
+        Args:
+            __name (str): The name of the attribute.
+            __value (Any): The value for the attribute.
+        """
+        if __name != "update_at":
+            self.__dict__["updated_at"] = datetime.now()
+            self.__dict__[__name] = __value
+
+    def save(self) -> None:
+        """Save the instance and updates the `updated_at`"""
+        self.updated_at = datetime.now()
+
         models.storage.save()
 
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
+    def to_dict(self) -> dict:
+        """Returns the dictionary containing all the key/values of `__dict__`
+        of the instance.
 
-        Includes the key/value pair __class__ representing
-        the class name of the object.
+        The `updated_at` and `created_at` instance attributes are converted to
+        ISO format. A new key named `__class__` is added to the dictionary.
+
+        Returns:
+            dict: The dictionary containing all the key/values of `__dict__`
+            of the instance.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        # copy the dictionary to avoid changing the original
+        obj_dict = dict(self.__dict__)
 
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        obj_dict["__class__"] = self.__class__.__name__
+
+        # update time to ISO format
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["created_at"] = self.created_at.isoformat()
+
+        return obj_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,49 +1,78 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+
+"""
+This module defines the File Storage class that serializes instances to JSON
+files deserializes JSON files to instances.
+"""
+
 import json
+from typing import Any
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
+from models.state import State
 from models.review import Review
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
+    """Defines the file storage model."""
 
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-    """
-    __file_path = "file.json"
+    __file_path = "file_storage.json"
     __objects = {}
+    __models = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review,
+    }
 
-    def all(self):
-        """Return the dictionary __objects."""
-        return FileStorage.__objects
+    def all(self) -> dict:
+        """
+        Returns all the objects in the dictionary
 
-    def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        Returns:
+            dict: A dictionary containing all serialized objects.
+        """
+        return self.__objects
 
-    def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+    def new(self, obj: Any) -> None:
+        """
+        Saves a new instance to the objects dictionary
 
-    def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        Args:
+            obj (Any): The object save in dictionary
+        """
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def reload(self) -> None:
+        """Deserializes the json objects into their respective models."""
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
+            with open(self.__file_path, "r", encoding="utf-8") as json_file:
+                instances = json.load(json_file)
+
+                for class_name_id, json_dict in instances.items():
+                    model_name = json_dict["__class__"]
+                    self.__objects[class_name_id] = self.__models[model_name](
+                        **json_dict
+                    )
+        except (FileNotFoundError, PermissionError):
+            pass
+
+    def save(self) -> None:
+        """Serializes the objects dictionary and save it to a JSON file."""
+        instances = {}
+
+        for class_id, obj in self.__objects.items():
+            # ensure valid keys
+            if class_id != f"{obj.__class__.__name__}.{obj.id}":
+                raise KeyError("invalid key. key must be <class name>.<id>")
+
+            instances[class_id] = obj.to_dict()
+
+        with open(self.__file_path, "w", encoding="utf-8") as json_file:
+            json.dump(instances, json_file, indent=4)
